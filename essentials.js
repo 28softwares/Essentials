cdae1bf94b21a9b8006167557e6109fa83ab1893
originalsaroj <sarojdahal1213@gmail.com>
2019-09-14 21:00:55 +0545

    Essentials.js and Essentials.js has been added

---
+++ essentials.js
@@ -0,0 +1,25 @@
+/*
+ Essentials.js is simple js code which contain all basic and essential javascript
+ functions for the website development. 
+ AUTHOR : Saroj Dahal 
+*/
+
+//Function for cookies
+
+function setCookie(key, value, expiry) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+function eraseCookie(key) {
+    var keyValue = getCookie(key);
+    setCookie(key, keyValue, '-1');
+}
+
+
