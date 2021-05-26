94d6f266151542a70df46078d377365400cfdaac
Saroj Dahal <sarojdahal1213@gmail.com>
2021-05-26 17:45:20 +0545

    added: convertDateToSQLFormat() method in essentials.js

--- javascript/essentials.js
+++ javascript/essentials.js
@@ -1,9 +1,17 @@
 /*
  Essentials.js is simple js code which contain all basic and essential javascript
  functions for the website development. 
- AUTHOR : Saroj Dahal / @originalsaroj
+ AUTHOR : Saroj Dahal / @isarojdahal
  VERSION: 1.0.0
 */
+
+function convertDateToSQLFormat(paramDate) {
+  // if date is not given, converts current date to SQL Format.
+  //  else the given data is convert to SQL format.
+  if (paramDate != null)
+    return paramDate.toISOString().slice(0, 19).replace("T", " ");
+  return new Date().toISOString().slice(0, 19).replace("T", " ");
+}
 
 function copyit(theField) {
   var selectedText = document.selection;
@@ -77,56 +85,53 @@ function redirectTo(url, seconds) {
   setTimeout(`window.location.href ='${url}'`, seconds * 1000);
 }
 
-
 //Playing With URL (Might be needed sometimes when URL needs to be used and played with using javascript)
 
 //Remove a particular parameter from a URL
 
 function removeURLParameter(url, parameter) {
-  url = url.split('#')[0];
-  var urlparts= url.split('?');
-  if (urlparts.length>=2) {
-
-      var prefix= encodeURIComponent(parameter)+'=';
-      var pars= urlparts[1].split(/[&;]/g);
-      for (var i= pars.length; i-- > 0;) {
-          if (pars[i].lastIndexOf(prefix, 0) !== -1) {
-              pars.splice(i, 1);
-          }
+  url = url.split("#")[0];
+  var urlparts = url.split("?");
+  if (urlparts.length >= 2) {
+    var prefix = encodeURIComponent(parameter) + "=";
+    var pars = urlparts[1].split(/[&;]/g);
+    for (var i = pars.length; i-- > 0; ) {
+      if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+        pars.splice(i, 1);
       }
-      url= urlparts[0]+'?'+pars.join('&');
-      return url;
+    }
+    url = urlparts[0] + "?" + pars.join("&");
+    return url;
   } else {
-      return url;
+    return url;
   }
 }
 
 //Add a particular or a list of parameters to a URL
 
-function addParams( url, data ) {
-    if ( ! $.isEmptyObject(data) )
-    {
-        url += ( url.indexOf('?') >= 0 ? '&' : '?' ) + $.param(data);
-    }
+function addParams(url, data) {
+  if (!$.isEmptyObject(data)) {
+    url += (url.indexOf("?") >= 0 ? "&" : "?") + $.param(data);
+  }
 
-    return url;
+  return url;
 }
 
 //Get a value of parameter from URL
 
 function getUrlParameter(sParam) {
   var sPageURL = window.location.search.substring(1),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
+    sURLVariables = sPageURL.split("&"),
+    sParameterName,
+    i;
 
   for (i = 0; i < sURLVariables.length; i++) {
-      sParameterName = sURLVariables[i].split('=');
+    sParameterName = sURLVariables[i].split("=");
 
-      if (sParameterName[0] === sParam) {
-          return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-      }
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined
+        ? true
+        : decodeURIComponent(sParameterName[1]);
+    }
   }
 }
-
-
